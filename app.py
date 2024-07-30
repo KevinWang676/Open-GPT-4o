b9d2a50ffe0f3f1c4eb95553d92d448287316a3f
Kevin Wang <126712357+KevinWang676@users.noreply.github.com>
2024-07-30 15:38:10 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,156 @@
+import os
+import re
+import librosa
+import gradio as gr
+
+import numpy as np
+import torch
+import torchaudio
+
+import spaces
+
+import torchvision.transforms as T
+from decord import VideoReader, cpu
+from PIL import Image
+from torchvision.transforms.functional import InterpolationMode
+from transformers import AutoModel, AutoTokenizer
+
+#sensevoice
+
+from funasr import AutoModel
+
+model = "FunAudioLLM/SenseVoiceSmall"
+model = AutoModel(model=model,
+				  vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+				  vad_kwargs={"max_single_segment_time": 30000},
+                  hub="hf",
+                  device="cuda"
+				  )
+
+@spaces.GPU
+def model_inference(input_wav, language, fs=16000):
+	# task_abbr = {"Speech Recognition": "ASR", "Rich Text Transcription": ("ASR", "AED", "SER")}
+	language_abbr = {"auto": "auto", "zh": "zh", "en": "en", "yue": "yue", "ja": "ja", "ko": "ko",
+					 "nospeech": "nospeech"}
+	
+	# task = "Speech Recognition" if task is None else task
+	language = "auto" if len(language) < 1 else language
+	selected_language = language_abbr[language]
+	# selected_task = task_abbr.get(task)
+	
+	# print(f"input_wav: {type(input_wav)}, {input_wav[1].shape}, {input_wav}")
+	
+	if isinstance(input_wav, tuple):
+		fs, input_wav = input_wav
+		input_wav = input_wav.astype(np.float32) / np.iinfo(np.int16).max
+		if len(input_wav.shape) > 1:
+			input_wav = input_wav.mean(-1)
+		if fs != 16000:
+			print(f"audio_fs: {fs}")
+			resampler = torchaudio.transforms.Resample(fs, 16000)
+			input_wav_t = torch.from_numpy(input_wav).to(torch.float32)
+			input_wav = resampler(input_wav_t[None, :])[0, :].numpy()
+	
+	
+	merge_vad = True #False if selected_task == "ASR" else True
+	print(f"language: {language}, merge_vad: {merge_vad}")
+	text = model.generate(input=input_wav,
+						  cache={},
+						  language=language,
+						  use_itn=True,
+						  batch_size_s=500, merge_vad=merge_vad)
+	
+	print(text)
+	text = text[0]["text"]
+  match = re.search(r'[^>]*$', text)
+  if match:
+    text = match.group(0)
+    print(text)
+	
+	return text
+
+# internvl
+
+IMAGENET_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_STD = (0.229, 0.224, 0.225)
+
+def build_transform(input_size):
+    MEAN, STD = IMAGENET_MEAN, IMAGENET_STD
+    transform = T.Compose([
+        T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
+        T.Resize((input_size, input_size), interpolation=InterpolationMode.BICUBIC),
+        T.ToTensor(),
+        T.Normalize(mean=MEAN, std=STD)
+    ])
+    return transform
+
+def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size):
+    best_ratio_diff = float('inf')
+    best_ratio = (1, 1)
+    area = width * height
+    for ratio in target_ratios:
+        target_aspect_ratio = ratio[0] / ratio[1]
+        ratio_diff = abs(aspect_ratio - target_aspect_ratio)
+        if ratio_diff < best_ratio_diff:
+            best_ratio_diff = ratio_diff
+            best_ratio = ratio
+        elif ratio_diff == best_ratio_diff:
+            if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
+                best_ratio = ratio
+    return best_ratio
+
+def dynamic_preprocess(image, min_num=1, max_num=12, image_size=448, use_thumbnail=False):
+    orig_width, orig_height = image.size
+    aspect_ratio = orig_width / orig_height
+
+    # calculate the existing image aspect ratio
+    target_ratios = set(
+        (i, j) for n in range(min_num, max_num + 1) for i in range(1, n + 1) for j in range(1, n + 1) if
+        i * j <= max_num and i * j >= min_num)
+    target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+
+    # find the closest aspect ratio to the target
+    target_aspect_ratio = find_closest_aspect_ratio(
+        aspect_ratio, target_ratios, orig_width, orig_height, image_size)
+
+    # calculate the target width and height
+    target_width = image_size * target_aspect_ratio[0]
+    target_height = image_size * target_aspect_ratio[1]
+    blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
+
+    # resize the image
+    resized_img = image.resize((target_width, target_height))
+    processed_images = []
+    for i in range(blocks):
+        box = (
+            (i % (target_width // image_size)) * image_size,
+            (i // (target_width // image_size)) * image_size,
+            ((i % (target_width // image_size)) + 1) * image_size,
+            ((i // (target_width // image_size)) + 1) * image_size
+        )
+        # split the image
+        split_img = resized_img.crop(box)
+        processed_images.append(split_img)
+    assert len(processed_images) == blocks
+    if use_thumbnail and len(processed_images) != 1:
+        thumbnail_img = image.resize((image_size, image_size))
+        processed_images.append(thumbnail_img)
+    return processed_images
+
+def load_image(image_file, input_size=448, max_num=12):
+    image = Image.open(image_file).convert('RGB')
+    transform = build_transform(input_size=input_size)
+    images = dynamic_preprocess(image, image_size=input_size, use_thumbnail=True, max_num=max_num)
+    pixel_values = [transform(image) for image in images]
+    pixel_values = torch.stack(pixel_values)
+    return pixel_values
+
+# If you have an 80G A100 GPU, you can put the entire model on a single GPU.
+# Otherwise, you need to load a model using multiple GPUs, please refer to the `Multiple GPUs` section.
+path = 'OpenGVLab/InternVL2-8B'
+model_vl = AutoModel.from_pretrained(
+    path,
+    torch_dtype=torch.bfloat16,
+    low_cpu_mem_usage=True,
+    trust_remote_code=True).eval().cuda()
+tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False)
